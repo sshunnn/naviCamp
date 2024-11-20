ee94d2bfc52fb74bf11a83bfe3717fb4303270d2
shunprog <shun.prog@gmail.com>
2024-11-21 01:46:54 +0900

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,70 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <>
+      <body>
+        <header className={styles.Header}>
+          <nav className={styles.Navigation_wrapper}>
+            <span className={styles.Navigation_logo}>NextCamp</span>
+            <form>
+              <input
+                type="text"
+                id="searchInput"
+                name="searchInput"
+                placeholder="キーワード・エリア・施設"
+                className={styles.Navigation_search}
+              />
+            </form>
+            <a href="/register" className={styles.Navigation_login}>
+              会員登録
+            </a>
+            <a href="/register" className={styles.Navigation_login}>
+              ログイン
+            </a>
+          </nav>
+          <h1 className={styles.Header_title}>
+            自然と触れ合い、新しい思い出を作ろう
+          </h1>
+        </header>
+        <div className={styles.Top}>
+          <Image
+            src="/topBackGround.jpg"
+            alt="Top Background"
+            width={1920} // 画像の幅を指定
+            height={450} // 画像の高さを指定
+            className={styles.Top_headImage}
+          />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <h1 className={styles.Top_heading}>キャンプ場一覧</h1>
+        <div className={styles.campCard_wrapper}>
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/camp.jpg"
+            alt="camp1"
+            width={210} // 画像の幅を指定
+            height={210} // 画像の高さを指定
+            // className={styles.Top_headImage}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <dl className={styles.campCard_additionalInfo}>
+            <dt className={styles.campCard_campName}>ネーブルキャンプ</dt>
+            <dd className={styles.campCard_detailInfo}>
+              <p className={styles.campCard_explanation}>
+                風の渡る音、鳥やカエルの声…それが最高のBGM♪農家の裏庭キャンプ場。
+              </p>
+              <p className={styles.campCard_explanation}>
+                田んぼの真ん中で静けさを楽しみませんか？ 
+              </p>
+              <p className={styles.campCard_address}>
+                埼玉県加須市油井ケ島1393-1
+              </p>
+              <a className={styles.campCard_link} href="">
+                ネーブルパークの詳細
+              </a>
+            </dd>
+          </dl>
+        </div>
+      </body>
+    </>
   );
 }
